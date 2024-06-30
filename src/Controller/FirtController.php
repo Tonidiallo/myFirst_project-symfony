48f49c12d239cc63f516a4948869903cbd82cc24
macbookpro <antoinediallo@gmail.com>
2024-06-30 14:29:17 +0000

    MAJ 30 juin 2024

--- src/Controller/FirtController.php
+++ src/Controller/FirtController.php
@@ -5,14 +5,45 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Config\router;
+use Symfony\Component\HttpFoundation\Request;
 
 class FirtController extends AbstractController
 {
     #[Route('/firt', name: 'app_firt')]
     public function index(): Response
     {
+        // chercher au la abse de données vos users
         return $this->render('firt/index.html.twig', [
-            'controller_name' => 'FirtController',
+            'name' => 'DIALLO',
+            'firstname' => 'ANTOINE'
         ]);
     }
+
+
+    #[Route('/sayHello/{name}/{firstname}', name: 'app_sayhello')]
+    public function sayHello(Request $request, $name, $firstname)
+    {
+        return $this->render('firt/hello.html.twig', [
+            'nom' => $name,
+            'prenom' => $firstname
+        ]);
+    }
+
+    #[Route(
+        '/multi/{entier1<\d+>}/{entier2<\d+>}',
+        name: 'app_multiplication',
+
+        )]
+    public function multiplication($entier1, $entier2){
+        $resultat = $entier1 * $entier2;
+        return new Response( "<h1>$resultat</h1>");
+
+    }
+
+    #[Route('/template',name: 'app_template')]
+    public function template(){
+        return $this->render('template.html.twig');
+    }
+
 }
--- src/Controller/TabController.php
+++ src/Controller/TabController.php
@@ -8,15 +8,27 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class TabController extends AbstractController
 {
-    #[Route('/tab/{nb}', name: 'app_tab')]
-    public function index($nb): Response
+    #[Route('/tab/{nb<\d+>?5}', name: 'tab')]
+    public function index($nb)
     {
-        $notes =[];
-        for($i=0;$i<$nb;$i++){
+        $notes = [];
+        for ($i = 0 ; $i<$nb ;$i++) {
             $notes[] = rand(0,20);
         }
         return $this->render('tab/index.html.twig', [
             'notes' => $notes,
+        ]);
+    }
+    #[Route('/tab/users', name: 'app_users')]
+    public function users()
+    {
+        $users = [
+            ['firstname' => 'aymen', 'name' => 'sellaouti', 'age' => 39],
+            ['firstname' => 'skander', 'name' => 'sellaouti', 'age' => 3],
+            ['firstname' => 'souheib', 'name' => 'youssfi', 'age' => 59],
+        ];
+        return $this->render('tab/users.html.twig', [
+            'users' => $users
         ]);
     }
 }
